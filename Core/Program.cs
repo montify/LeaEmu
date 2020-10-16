3b7fcddccbc752e38d5c4339b5fa0097674e8008
Alex <montify@gmail.com>
2020-10-16 19:18:06 +0200

    Revert "Revert"
    This reverts commit 52a2a1355be6faa6e8831bac2f21dafff48d3abe.

--- Core/Program.cs
+++ Core/Program.cs
@@ -8,19 +8,6 @@ namespace Core
 	{
 		static void Main(string[] args)
 		{
-<<<<<<< HEAD
-=======
-			var cpu = new Cpu();
-			cpu.Execute(new Instruction(CPUOpCode.LDA, 0xFA, CPUAdressingMode.Immediate)); // Load 0x80 in REG_A
-			cpu.Execute(new Instruction(CPUOpCode.STA, 0x01, CPUAdressingMode.Immediate)); // Store value from REG_A in memory[0x01] 
-            cpu.Execute(new Instruction(CPUOpCode.ADC, 0x01, CPUAdressingMode.Immediate)); // Store value from REG_A in memory[0x01] 
-              cpu.Execute(new Instruction(CPUOpCode.STA, 0xAE, CPUAdressingMode.Immediate)); // Store value from REG_A in memory[0x01] 
-            
-            
-		
-
-
->>>>>>> parent of bc4a1b0... Add TestSln
 
 			Console.Read();
 		}
